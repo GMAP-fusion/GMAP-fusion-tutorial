8d11925bfccd2095896fd8951ea102aa8ff263b7
Brian Haas <brianjohnhaas@gmail.com>
2018-03-05 21:00:15 -0500

    update

--- .__runMe.sh
+++ .__runMe.sh
@@ -5,3 +5,5 @@ set -ev
 # prep genome lib
 ../FusionFilter/prep_genome_lib.pl --genome_fa minigenome.fa --gtf minigenome.gtf --fusion_annot_lib CTAT_HumanFusionLib.mini.dat.gz --annot_filter_rule AnnotFilterRule.pm --gmap_build
 
+# run gmap-fusion
+../GMAP-fusion -T discasm.Trinity.fasta --genome_lib_dir ctat_genome_lib_build_dir --left_fq ./rnaseq_1.fastq.gz --right_fq ./rnaseq_2.fastq.gz
